4d3260479c193948ecd6937f62d45f93acbf2287
Alexis Pojomovsky <apojomovsky@gmail.com>
2026-09-16 10:26:47 -0300

    feat(pic16f87xa-hal): onboard cut-down 16F871 to the 87XA family

--- pic16f87xa-hal/include/pic16f87xa.h
+++ pic16f87xa-hal/include/pic16f87xa.h
@@ -1,8 +1,8 @@
 /* PIC16F87XA top-level entry: standard types, status codes, build-time
  * device selection, and the SFR mapping layer. DS39582B is authoritative
  * for every constant; each peripheral header cites its own section.
- * Device (DS39582B §1.0, Table 1-1; non-A 870/873/874/876/877 per
- * DS30529): 870/873/873A/874/874A/876/876A/877/877A, 2/4/8 KW flash,
+ * Device (DS39582B §1.0, Table 1-1; non-A 870/871/873/874/876/877
+ * per DS30529): 870/871/873/873A/874/874A/876/876A/877/877A, 2/4/8 KW flash,
  * 128/192/368 B RAM, 64/128/256 B EEPROM, 5/8 ADC channels. */
 
 #ifndef PIC16F87XA_H
@@ -22,17 +22,17 @@
  *            peripheral header. Defaults to PIC16F877A when none is set.
  * @{
  */
-#if !defined(PIC16F870) && !defined(PIC16F873) && !defined(PIC16F873A) && \
+#if !defined(PIC16F870) && !defined(PIC16F871) && !defined(PIC16F873) && !defined(PIC16F873A) && \
     !defined(PIC16F874) && !defined(PIC16F874A) && \
     !defined(PIC16F876) && !defined(PIC16F876A) && \
     !defined(PIC16F877) && !defined(PIC16F877A)
 #define PIC16F877A   1
 #endif
 
-#if defined(PIC16F870) + defined(PIC16F873) + defined(PIC16F873A) + defined(PIC16F874) + \
+#if defined(PIC16F870) + defined(PIC16F871) + defined(PIC16F873) + defined(PIC16F873A) + defined(PIC16F874) + \
     defined(PIC16F874A) + defined(PIC16F876) + defined(PIC16F876A) + \
     defined(PIC16F877) + defined(PIC16F877A) > 1
-#error "Define exactly one of PIC16F870 / PIC16F873 / PIC16F873A / PIC16F874 / PIC16F874A / PIC16F876 / PIC16F876A / PIC16F877 / PIC16F877A."
+#error "Define exactly one of PIC16F870 / PIC16F871 / PIC16F873 / PIC16F873A / PIC16F874 / PIC16F874A / PIC16F876 / PIC16F876A / PIC16F877 / PIC16F877A."
 #endif
 
 #if   defined(PIC16F870)
@@ -49,6 +49,20 @@
   #define PIC16F87XA_FAMILY_HAS_PORTE  0
   #define PIC16F87XA_FAMILY_HAS_PSP    0
   #define PIC16F87XA_DEVICE_NAME       "PIC16F870"
+#elif defined(PIC16F871)
+  #define PIC16F87XA_FAMILY_FLASH_KW   2
+  #define PIC16F87XA_FAMILY_HAS_USART  1
+  #define PIC16F87XA_FAMILY_HAS_SSP    0
+  #define PIC16F87XA_FAMILY_HAS_CCP2   0
+  #define PIC16F87XA_FAMILY_HAS_COMP   0
+  #define PIC16F87XA_FAMILY_HAS_VREF   0
+  #define PIC16F87XA_FAMILY_RAM_BYTES  128
+  #define PIC16F87XA_FAMILY_EEPROM_B   64
+  #define PIC16F87XA_FAMILY_ADC_CH     8
+  #define PIC16F87XA_FAMILY_HAS_PORTD  1
+  #define PIC16F87XA_FAMILY_HAS_PORTE  1
+  #define PIC16F87XA_FAMILY_HAS_PSP    1
+  #define PIC16F87XA_DEVICE_NAME       "PIC16F871"
 #elif defined(PIC16F873)
   #define PIC16F87XA_FAMILY_FLASH_KW   4
   #define PIC16F87XA_FAMILY_HAS_USART  1
